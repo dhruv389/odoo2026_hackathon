1e05609e4750e44dd8fed85b3335efa83a9f58c1
Ronak Sharma <rsvk1819@gmail.com>
2026-02-21 10:47:22 +0530

    feat : Add user routes ,controllers and model

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,17 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
-import userRoutes from './routes/userRoutes.js';
+import userRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+ 
 // Database connection
 connectDB();
 
